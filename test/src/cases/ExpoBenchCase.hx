224367a1d792fa392bd6a840830de011d15f9f22
shohei909 <909mmsphere@gmail.com>
2016-11-27 14:34:49 +0900

    add cpp test

--- test/src/cases/ExpoBenchCase.hx
+++ test/src/cases/ExpoBenchCase.hx
@@ -10,6 +10,8 @@ class ExpoBenchCase extends NanoTestCase {
         trace(".NET version: ", cs.system.Environment.Version);
         #end
         
+        var t = 1 / 5;
+            
         var p = [];
         var e = [];
         
@@ -21,6 +23,14 @@ class ExpoBenchCase extends NanoTestCase {
         
         trace("Pow M:" + mean(p) + " SD:" + standardDeviation(p));
         trace("Exp M:" + mean(e) + " SD:" + standardDeviation(e));
+    }
+    
+    public function test() {
+        var len = 20;
+        for (i in 0...len) {
+            var t = i / (len - 1);
+            this.assertTrue(Math.abs(Math.pow(2, 10 * (t - 1)) - Math.exp(LN_2_10 * (t - 1))) < 0.000000000001).label(i);
+        }
     }
     
     public function standardDeviation(p:Array<Float>) {
@@ -47,37 +57,38 @@ class ExpoBenchCase extends NanoTestCase {
     
     public function tracePow():Float
     {
-        var time = Date.now().getTime();
+        var time = getTime();
         var v = 0.0;
         for (i in 0...L) {
             var t = i / L;
             v = Math.pow(2, 10 * (t - 1));
         }
         
-        var result = (Date.now().getTime() - time);
+        var result = getTime() - time;
         trace("Pow:" + v + ":"  + result);
         return result;
     }
     
     public function traceExp()
     {
-        var time = Date.now().getTime();
+        var time = getTime();
         var v = 0.0;
         for (i in 0...L) {
             var t = i / L;
             v = Math.exp(LN_2_10 * (t - 1));
         }
         
-        var result = (Date.now().getTime() - time);
+        var result = getTime() - time;
         trace("Exp:" + v + ":"  + result);
         return result;
     }
     
-    public function test() {
-        var len = 20;
-        for (i in 0...len) {
-            var t = i / (len - 1);
-            this.assertTrue(Math.abs(Math.pow(2, 10 * (t - 1)) - Math.exp(LN_2_10 * (t - 1))) < 0.000000000001).label(i);
-        }
+    private static function getTime():Float
+    {
+        #if sys
+        return Sys.cpuTime() * 1000;
+        #else
+        return Date.now().getTime();
+        #end
     }
 }
